56e32c1846cc69a2298f8b7b12b18c5e26ea0ab7
Gokulbharathi S <gokulbs0023@gmail.com>
2025-11-03 21:10:06 +0530

    Import and export, inline testing

--- src/sum.ts
+++ src/sum.ts
@@ -1,3 +1,19 @@
 export default function sum(...numbers: number[]) {
   return numbers.reduce((total, number) => total + number, 0);
 }
+
+if (import.meta.vitest) {
+  const { describe, expect, it } = import.meta.vitest;
+
+  describe("#sum", () => {
+    it("return 0 with no numbers", () => {
+      expect(sum()).toBe(0);
+    });
+    it("return same number with one number", () => {
+      expect(sum(10)).toBe(10);
+    });
+    it("return sum with multiple numbers", () => {
+      expect(sum(10, 20, 30)).toBe(60);
+    });
+  });
+}
